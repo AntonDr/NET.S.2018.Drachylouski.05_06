573bfb18a49382b88a421abedc99fc65d3891942
AntonDr <31773079+AntonDr@users.noreply.github.com>
2018-07-07 02:08:47 +0300

    07.07 deadline done

---
+++ NET.S.2018.Drachylouski.05_06/ConverterLogic/Converter.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterLogic
+{    
+    public static class Converter
+    {
+        /// <summary>
+        /// To the decimal converter.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="ground">The ground.</param>
+        /// <returns>The number.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static long ToDecimalConverter(this string source, int ground)
+        {
+            var notation = new Notation(ground);
+
+            Validate(source,notation);
+
+            long number = 0, product = 1;
+
+            int @base = notation.Base;
+
+            string alphabet = notation.Alphabet;
+
+            string upperString = source.ToUpper();
+
+            for (int i = source.Length - 1; i >= 0; i--)
+            {
+                checked
+                {
+                    if (ConvertToValue(upperString[i], alphabet) == -1)
+                    {
+                        throw new ArgumentException($"Invalid symbol {source[i]} in string!");
+                    }
+                    else
+                    {
+                        number += product * ConvertToValue(upperString[i], alphabet);
+
+                        product *= @base;
+                    }
+                }
+            }
+
+            return number;
+        }
+
+        /// <summary>
+        /// Validates the specified source.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="notation">The notation.</param>
+        /// <exception cref="ArgumentException">source</exception>
+        /// <exception cref="ArgumentNullException">notation</exception>
+        private static void Validate(string source, Notation notation)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                throw new ArgumentException($"The string {nameof(source)} can not be null or empty!");
+            }
+
+            if (notation == null)
+            {
+                throw new ArgumentNullException($"The object {nameof(notation)} can not be null!");
+            }
+        }
+
+        /// <summary>
+        /// Converts to value.
+        /// </summary>
+        /// <param name="symbol">The symbol.</param>
+        /// <param name="alphbet">The alphbet.</param>
+        /// <returns>Value of current symbol</returns>
+        private static int ConvertToValue(char symbol, string alphbet) => alphbet.IndexOf(symbol);
+    }
+
+    /// <summary>
+    /// Сlass that connects the ground with its alphabet
+    /// </summary>
+    public sealed class Notation
+    {
+        #region Constants
+
+        private const int UpperValue = 16;
+        private const int LowerValue = 2;
+
+        #endregion
+
+        #region Fields
+
+        private int @base;
+
+        #endregion
+
+        #region Constructors
+
+        public Notation()
+            : this(10)
+        {
+        }
+
+        public Notation(int @base)
+        {
+            this.Base = @base;
+            this.Alphabet = this.StringCreation();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Base
+        {
+            get => this.@base;
+
+            set
+            {
+                if (value < LowerValue || value > UpperValue)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        $"Base of Notation must be more or equal then {LowerValue} and less or equal then {UpperValue}!");
+                }
+
+                this.@base = value;
+                this.Alphabet = this.StringCreation();
+            }
+        }
+
+        public string Alphabet { get; private set; }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Сreates an alphabet for the base
+        /// </summary>
+        /// <returns></returns>
+        private string StringCreation()
+        {
+            int @base = this.Base;
+
+            StringBuilder sb = new StringBuilder(@base);
+
+            int symbol = 'A';
+
+            for (int i = 0; i < @base; i++)
+            {
+                if (i <= 9)
+                {
+                    sb.Append(i);
+                }
+                else
+                {
+                    sb.Append((char) symbol++);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        #endregion
+
+    }
+}
---
+++ NET.S.2018.Drachylouski.05_06/NUnitConverterTest/ConverterTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnitConverterTest
+{
+    using ConverterLogic;
+
+    [TestFixture]
+    public class ConverterTest
+    {
+        [TestCase("0110111101100001100001010111111", 2, ExpectedResult = 934331071)]
+        [TestCase("01101111011001100001010111111", 2, ExpectedResult = 233620159)]
+        [TestCase("11101101111011001100001010", 2, ExpectedResult = 62370570)]
+        [TestCase("1AeF101", 16, ExpectedResult = 28242177)]
+        [TestCase("1ACB67", 16, ExpectedResult = 1756007)]
+        [TestCase("764241", 8, ExpectedResult = 256161)]
+        [TestCase("10", 5, ExpectedResult = 5)]
+        public long NUitTestCase(string source, int n) => source.ToDecimalConverter(n);
+
+
+        [TestCase("1ACB67", 2)]
+        [TestCase("SA123", 16)]
+        public void NUnitExceptionTestCase1(string source, int n)
+        {
+            Assert.Throws<ArgumentException>(() => source.ToDecimalConverter(n));
+        }
+
+        [TestCase("73321", 20)]
+        [TestCase("123", -4)]
+        public void NUnitExceptionTestCase2(string source, int n)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => source.ToDecimalConverter(n));
+        }
+    }
+}
